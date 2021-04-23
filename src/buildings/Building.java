07535be549747cfa5734df52321b35fa1336c77f
aboueleyes <ibrahimabouelanin@gmail.com>
2021-04-23 17:31:55 +0200

    Add Bulding Package clasess
    implementing the attributes of the classes, fix the hireachy.also
    implemented constructors and getters and setters.

--- src/buildings/Building.java
+++ src/buildings/Building.java
@@ -1,5 +1,46 @@
 package buildings;
 
-public class Building {
+public abstract class Building {
 
+    private int cost; // The cost for creating a buliding. READ ONLY
+    private int level; // The current level of the bulding.
+    private int upgradeCost; // The cost for upgrading building’s level.
+    private boolean coolDown = true; // variable stating if the building is cooling down.
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getUpgradeCost() {
+        return upgradeCost;
+    }
+
+    public void setUpgradeCost(int upgradeCost) {
+        this.upgradeCost = upgradeCost;
+    }
+
+    public boolean isCoolDown() {
+        return coolDown;
+    }
+
+    public void setCoolDown(boolean coolDown) {
+        this.coolDown = coolDown;
+    }
+
+    protected Building() {
+
+    }
+
+    public Building(int cost, int upgradeCost) {
+        this.cost = cost;
+        this.upgradeCost = upgradeCost;
+    }
 }
--- src/buildings/MilitaryBuilding.java
+++ src/buildings/MilitaryBuilding.java
@@ -1,5 +1,44 @@
 package buildings;
 
-public class MilitaryBuilding {
+public abstract class MilitaryBuilding extends Building {
 
+    private int recruitmentCost; // The cost for recruiting a unit.
+    private int currentRecruit; // Current number of units recruited by a building inside a turn
+
+    /**
+     * This constant should be static final one. no point of doing getter and
+     * encupclation it also should follow constant conventin naming MAX_RECRUIT; but
+     * we have no choice ;(
+     */
+    private final int maxRecruit = 3; // Maximum number of units a building can recruit per turn
+
+    public int getRecruitmentCost() {
+        return recruitmentCost;
+    }
+
+    public void setRecruitmentCost(int recruitmentCost) {
+        this.recruitmentCost = recruitmentCost;
+    }
+
+    public int getCurrentRecruit() {
+        return currentRecruit;
+    }
+
+    public void setCurrentRecruit(int currentRecruit) {
+        this.currentRecruit = currentRecruit;
+    }
+
+    public int getMaxRecruit() {
+        return maxRecruit;
+    }
+    public MilitaryBuilding(){
+
+    } 
+    /**
+     *  This also should be protected since no point of public constrctor 
+     */
+    public MilitaryBuilding(int cost, int upgradeCost, int recruitmentCost) {
+        super(cost, upgradeCost);
+        this.recruitmentCost = recruitmentCost;
+    }
 }
