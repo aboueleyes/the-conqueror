d27ecd1ebbfbe216f2555344656d19364041e777
aboueleyes <ibrahimabouelanin@gmail.com>
2021-04-23 21:39:36 +0200

    Refactor building package
    convert all files to use 2 spaces instead of 4

--- src/buildings/Barracks.java
+++ src/buildings/Barracks.java
@@ -2,11 +2,11 @@ package buildings;
 
 public class Barracks extends MilitaryBuilding {
 
-    private static final int BARRACKS_COST = 2000;
-    private static final int BARRACKS_UPGRADE_COST = 1000;
-    private static final int BARRACKS_RECRUITMENT_COST = 500;
+  private static final int BARRACKS_COST = 2000;
+  private static final int BARRACKS_UPGRADE_COST = 1000;
+  private static final int BARRACKS_RECRUITMENT_COST = 500;
 
-    public Barracks() {
-        super(BARRACKS_COST, BARRACKS_UPGRADE_COST, BARRACKS_RECRUITMENT_COST);
-    }
+  public Barracks() {
+    super(BARRACKS_COST, BARRACKS_UPGRADE_COST, BARRACKS_RECRUITMENT_COST);
+  }
 }
--- src/buildings/Farm.java
+++ src/buildings/Farm.java
@@ -1,10 +1,11 @@
 package buildings;
 
 public class Farm extends EconomicBuilding {
-    private static final int FARM_COST = 1000;
-    private static final int FARM_UPGRADE_COST = 500;
 
-    public Farm() {
-        super(FARM_COST, FARM_UPGRADE_COST);
-    }
+  private static final int FARM_COST = 1000;
+  private static final int FARM_UPGRADE_COST = 500;
+
+  public Farm() {
+    super(FARM_COST, FARM_UPGRADE_COST);
+  }
 }
