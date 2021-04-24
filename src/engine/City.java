4c7407adb7e58fad97a74d4a92c6130485dfcb5e
shaarawy29 <ashaarawy19@gmail.com>
2021-04-24 03:33:35 +0200

    impement the classess City, Distance and Player with their attributes, constructors, getters and setters

--- src/engine/City.java
+++ src/engine/City.java
@@ -1,5 +1,61 @@
 package engine;
 
-public class City {
+import java.util.ArrayList;
 
+import buildings.EconomicBuilding;
+import buildings.MilitaryBuilding;
+import units.Army;
+
+public class City {
+	
+  private String name; // The name of the city, READ  ONLY
+  private ArrayList<EconomicBuilding> economicalBuildings; //An ArrayList containing the economical building in the city, READ ONLy
+  private ArrayList<MilitaryBuilding> militaryBuildings; //An ArrayList containing the military building in the city, READ ONLY
+  private Army defendingArmy; // The defending army of the city
+  private int turnsUnderSiege; // Number of turns the city has been siege
+  private boolean underSiege = false; // Variable checking if the is under siege or not
+
+  public String getName() {
+	  return this.name;
+  }
+  
+  public ArrayList<EconomicBuilding> getEconomicalBuildings() {
+	  return this.economicalBuildings;
+  }
+  
+  public ArrayList<MilitaryBuilding> getMilitaryBuildings(){
+	  return this.militaryBuildings;
+  }
+  
+  public Army getDefendignArmy() {
+	  return this.defendingArmy;
+  }
+  
+  public void setDefendingArmy(Army defendingArmy) {
+	  this.defendingArmy = defendingArmy;
+  }
+  
+  public int getTurnsUnderSiege() {
+	  return this.turnsUnderSiege;
+  }
+  
+  public void setTurnsUnderSiege(int trunsUnderSiege) {
+	  this.turnsUnderSiege = trunsUnderSiege;
+  }
+  
+  public boolean isUnderSiege() {
+	  return this.underSiege;
+  }
+  
+  public void setUnderSiege(boolean underSiege) {
+	  this.underSiege = underSiege;
+  }
+  
+  public City(String name) {
+	  this.name = name;
+	  economicalBuildings = new ArrayList<>();
+	  militaryBuildings = new ArrayList<>();
+	  defendingArmy = new Army("");
+  }
+  
 }
--- src/engine/Player.java
+++ src/engine/Player.java
@@ -1,5 +1,49 @@
 package engine;
 
+import java.util.ArrayList;
+
+import units.Army;
+
 public class Player {
+	
+  private String name; //Variable representing the name of the player, READ ONLY
+  private ArrayList<City> controlledCiteis; // An ArrayList containing the player’s controlled cities, READ ONLY
+  private ArrayList<Army> controlledArmies; //An ArrayList containing the player’s controlled armies, READ ONLY
+  double treasury; //The amount of gold the player has
+  double food; //The amount of food the player has
+  
+  public String getName() {
+	  return this.name;
+  }
+  
+  public ArrayList<City> getControlledCities(){
+	  return this.controlledCiteis;
+  }
+  
+  public ArrayList<Army> getControlledArmies(){
+	  return this.controlledArmies;
+  }
+  
+  public double getTreasury() {
+	  return this.treasury;
+  }
+  
+  public void setTreasury(double treasury) {
+	  this.treasury = treasury;
+  }
+  
+  public double getFood() {
+	  return this.food;
+  }
+  
+  public void setFood(double food) {
+	  this.food = food;
+  }
+  
+  public Player(String name) {
+	  this.name = name;
+	  controlledCiteis = new ArrayList<>();
+	  controlledArmies = new ArrayList<>();
+  }
 
 }
