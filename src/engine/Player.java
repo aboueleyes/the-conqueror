1dd2d97df96e9ea19f8fc5c12885824e988c7ebe
shaarawy29 <ashaarawy19@gmail.com>
2021-04-24 14:26:34 +0200

    Fix the name of attribute in Player class and Fix the access modifier for other instance variablses

--- src/engine/Player.java
+++ src/engine/Player.java
@@ -7,17 +7,17 @@ import units.Army;
 public class Player {
 	
   private String name; //Variable representing the name of the player, READ ONLY
-  private ArrayList<City> controlledCiteis; // An ArrayList containing the player’s controlled cities, READ ONLY
+  private ArrayList<City> controlledCities; // An ArrayList containing the player’s controlled cities, READ ONLY
   private ArrayList<Army> controlledArmies; //An ArrayList containing the player’s controlled armies, READ ONLY
-  double treasury; //The amount of gold the player has
-  double food; //The amount of food the player has
+  private double treasury; //The amount of gold the player has
+  private double food; //The amount of food the player has
   
   public String getName() {
 	  return this.name;
   }
   
   public ArrayList<City> getControlledCities(){
-	  return this.controlledCiteis;
+	  return this.controlledCities;
   }
   
   public ArrayList<Army> getControlledArmies(){
@@ -42,7 +42,7 @@ public class Player {
   
   public Player(String name) {
 	  this.name = name;
-	  controlledCiteis = new ArrayList<>();
+	  controlledCities = new ArrayList<>();
 	  controlledArmies = new ArrayList<>();
   }
 
