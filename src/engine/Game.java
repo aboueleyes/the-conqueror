151d81157b73ed704540359bffe6e487f804e238
ShimaaBetah <shimaabetah1911@gmail.com>
2021-04-27 18:11:55 +0200

    Add loadCitiesAndDistances method and a primary constructor
    import ReadingCSVFile class , then use the method readFile to read distances.csv file
    create Distance object and add them to distances list attribute
    implement method called addedCity to check if the city we read is added before to availableCities list
    implement a primary sekeleton for constructor which just initialize attributes

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,6 +1,9 @@
 package engine;
 
+import utlis.ReadingCSVFile;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
 
@@ -38,6 +41,40 @@ public class Game {
 
   public void setCurrentTurnCount(int currentTurnCount) {
     this.currentTurnCount = currentTurnCount;
+  }
+
+  public Game(String playerName, String cityName) throws IOException{
+    this.player = new Player(playerName);
+    distances = new ArrayList<>();
+  }
+
+  // Load distances.csv file with format from,to,distance and initialise distances,availableCities attributes
+  public void loadCitiesAndDistances() throws IOException{
+    List<List<String>> data = ReadingCSVFile.readFile("distances.csv");
+    
+    for (List<String> line : data){
+      String from = line.get(0);
+      String to = line.get(1);
+      int distance = Integer.parseInt(line.get(2));
+      distances.add(new Distance(from, to, distance));
+
+      if (!addedCity(from)) {
+        availableCities.add(new City(from));
+      }
+      if (!addedCity(to)) {
+        availableCities.add(new City(to));
+      }
+    }
+  }
+
+  // Check if a City is added before by comparing the name of the city with all cities in availableCities List
+  public boolean addedCity(String cityName) {
+    for (City city : availableCities) {
+      if (city.getName().equals(cityName)) {
+        return true;
+      }
+    }
+    return false;
   }
 
 }
