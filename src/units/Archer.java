3e08241b23fbd55be45efcd19c930a6b5dd210a5
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2021-05-30 06:34:17 +0200

    Fix Archer class
    change modifier to private.

--- src/units/Archer.java
+++ src/units/Archer.java
@@ -4,9 +4,9 @@ public class Archer extends Unit {
 
   private static final double[][] ARCHER_VAlUES = { { 60, 0.4, 0.5, 0.6 }, { 60, 0.4, 0.5, 0.6 },
       { 70, 0.5, 0.6, 0.7 } };
-  protected static final double[] ARCHER_TARGET_FACTORS = { 0.3, 0.4, 0.5 };
-  protected static final double[] INFANTRY_TARGET_FACTORS = { 0.2, 0.3, 0.4 };
-  protected static final double[] CAVALRY_TARGET_FACTORS = { 0.1, 0.1, 0.2 };
+  private static final double[] ARCHER_TARGET_FACTORS = { 0.3, 0.4, 0.5 };
+  private static final double[] INFANTRY_TARGET_FACTORS = { 0.2, 0.3, 0.4 };
+  private static final double[] CAVALRY_TARGET_FACTORS = { 0.1, 0.1, 0.2 };
 
   public Archer(int level, int maxSoldierCount, double idleUpkeep, double marchingUpkeep, double siegeUpkeep) {
     super(level, maxSoldierCount, idleUpkeep, marchingUpkeep, siegeUpkeep);
