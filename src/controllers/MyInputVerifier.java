b0bf28f048fbb1990fa095716bb5c0ebf97e6825
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2021-06-14 10:30:07 +0200

    Add Valid input error
    give error messages

--- src/controllers/MyInputVerifier.java
+++ src/controllers/MyInputVerifier.java
@@ -6,17 +6,17 @@ import static javax.swing.JOptionPane.showMessageDialog;
 import javax.swing.JTextField;
 
 public class MyInputVerifier extends InputVerifier {
-  public static final int MAX_NAME_SIZE = 20;
+  public static final int MAX_NAME_SIZE = 35;
 
   @Override
   public boolean verify(JComponent input) {
     String text = ((JTextField) input).getText();
     if (text.length() > MAX_NAME_SIZE) {
-      showMessageDialog(null, "ERROR");
+      showMessageDialog(null, "Name is TOO long");
       return false;
     }
     if (text.isBlank()) {
-      showMessageDialog(null, "ERROR");
+      showMessageDialog(null, "Name cannot Blank ");
       return false;
     }
     return true;
